0fef4788cf72af22a254660675bf2b6634c6212a
Taras Pustovyi <tpustovyi@gk-software.com>
2026-03-22 00:13:41 +0100

    injection-solution: implement inversion of control solution

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -1,5 +1,14 @@
 package mate.academy.lib;
 
+import java.lang.reflect.Field;
+import java.util.Map;
+import mate.academy.service.FileReaderService;
+import mate.academy.service.ProductParser;
+import mate.academy.service.ProductService;
+import mate.academy.service.impl.FileReaderServiceImpl;
+import mate.academy.service.impl.ProductParserImpl;
+import mate.academy.service.impl.ProductServiceImpl;
+
 public class Injector {
     private static final Injector injector = new Injector();
 
@@ -8,6 +17,51 @@ public class Injector {
     }
 
     public Object getInstance(Class<?> interfaceClazz) {
-        return null;
+        Object clazzImplementationInstance = null;
+        Class<?> clazz = getImplClass(interfaceClazz);
+        if (clazz == null) {
+            throw new RuntimeException("Class does not exist. Can't create instance of "
+                    + interfaceClazz.getName());
+        }
+
+        if (clazz.isAnnotationPresent(Component.class)) {
+            clazzImplementationInstance = createInstance(clazz);
+        }
+
+        Field[] fields = clazz.getDeclaredFields();
+        for (Field field : fields) {
+            if (field.isAnnotationPresent(Inject.class)) {
+                Object instance = injector.getInstance(field.getType());
+
+                clazzImplementationInstance = createInstance(clazz);
+                field.setAccessible(true);
+                try {
+                    field.set(clazzImplementationInstance, instance);
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException("Can't create instance of " + clazz.getName(), e);
+                }
+            }
+            if (clazzImplementationInstance == null) {
+                clazzImplementationInstance = createInstance(clazz);
+            }
+        }
+        return clazzImplementationInstance;
+    }
+
+    private Object createInstance(Class<?> clazz) {
+        try {
+            return clazz.getConstructor().newInstance();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't create instance of " + clazz.getName(), e);
+        }
+    }
+
+    private Class<?> getImplClass(Class<?> interfaceClazz) {
+        Map<Class<?>, Class<?>> interfaceImpls = Map.of(
+                ProductParser.class, ProductParserImpl.class,
+                FileReaderService.class, FileReaderServiceImpl.class,
+                ProductService.class, ProductServiceImpl.class
+        );
+        return interfaceImpls.get(interfaceClazz);
     }
 }
