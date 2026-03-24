82818fb53e7a3ab9daa4b6b41157fb0676ce984e
Taras Pustovyi <tpustovyi@gk-software.com>
2026-03-24 14:29:48 +0100

    injection-solution: improve solution. introduce static interfaces, exception in case no component class

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -1,6 +1,7 @@
 package mate.academy.lib;
 
 import java.lang.reflect.Field;
+import java.util.HashMap;
 import java.util.Map;
 import mate.academy.service.FileReaderService;
 import mate.academy.service.ProductParser;
@@ -11,29 +12,38 @@ import mate.academy.service.impl.ProductServiceImpl;
 
 public class Injector {
     private static final Injector injector = new Injector();
+    private static final Map<Class<?>, Class<?>> INTERFACE_IMPLS = Map.of(
+            ProductParser.class, ProductParserImpl.class,
+            FileReaderService.class, FileReaderServiceImpl.class,
+            ProductService.class, ProductServiceImpl.class
+    );
+    private final Map<Class<?>, Object> instances = new HashMap<>();
 
     public static Injector getInjector() {
         return injector;
     }
 
     public Object getInstance(Class<?> interfaceClazz) {
-        Object clazzImplementationInstance = null;
         Class<?> clazz = getImplClass(interfaceClazz);
         if (clazz == null) {
             throw new RuntimeException("Class does not exist. Can't create instance of "
                     + interfaceClazz.getName());
         }
 
-        if (clazz.isAnnotationPresent(Component.class)) {
-            clazzImplementationInstance = createInstance(clazz);
+        if (instances.containsKey(clazz)) {
+            return instances.get(clazz);
         }
+
+        if (!clazz.isAnnotationPresent(Component.class)) {
+            throw new RuntimeException("Class " + clazz.getName() + " is not a component");
+        }
+
+        Object clazzImplementationInstance = createInstance(clazz);
 
         Field[] fields = clazz.getDeclaredFields();
         for (Field field : fields) {
             if (field.isAnnotationPresent(Inject.class)) {
                 Object instance = injector.getInstance(field.getType());
-
-                clazzImplementationInstance = createInstance(clazz);
                 field.setAccessible(true);
                 try {
                     field.set(clazzImplementationInstance, instance);
@@ -41,27 +51,25 @@ public class Injector {
                     throw new RuntimeException("Can't create instance of " + clazz.getName(), e);
                 }
             }
-            if (clazzImplementationInstance == null) {
-                clazzImplementationInstance = createInstance(clazz);
-            }
         }
         return clazzImplementationInstance;
     }
 
     private Object createInstance(Class<?> clazz) {
+        if (instances.containsKey(clazz)) {
+            return instances.get(clazz);
+        }
+
         try {
-            return clazz.getConstructor().newInstance();
-        } catch (Exception e) {
+            Object instance = clazz.getConstructor().newInstance();
+            instances.put(clazz, instance);
+            return instance;
+        } catch (ReflectiveOperationException e) {
             throw new RuntimeException("Can't create instance of " + clazz.getName(), e);
         }
     }
 
     private Class<?> getImplClass(Class<?> interfaceClazz) {
-        Map<Class<?>, Class<?>> interfaceImpls = Map.of(
-                ProductParser.class, ProductParserImpl.class,
-                FileReaderService.class, FileReaderServiceImpl.class,
-                ProductService.class, ProductServiceImpl.class
-        );
-        return interfaceImpls.get(interfaceClazz);
+        return INTERFACE_IMPLS.get(interfaceClazz);
     }
 }
